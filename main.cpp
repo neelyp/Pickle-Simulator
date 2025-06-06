9a0c39f4e5355114e8e596dd6c4fe01a05cd422c
neelyp <neel.pandruvada@mcvts.org>
2025-06-05 23:00:30 -0400

    setup header

--- main.cpp
+++ main.cpp
@@ -2,26 +2,14 @@
 //Final Project
 //6-3-25
 #include <iostream>
+#include "main.h"
+
 using namespace std;
 
 
 int main()
 {
-    struct {
-        double munyun = 100.00;
-        int sticks = 0;
-        int pickles = 0;
-        int jars = 0;
-        int batter = 0;
-    } inventory;
     
-    struct {
-        int day = 1;
-        double picklePrice = 0.5;
-        double jarPrice = 1.0;
-        double batterPrice = 0.27;
-    } game;
-
     cout << "Start your papas pickleria!";
     
     
